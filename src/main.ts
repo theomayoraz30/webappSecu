541108931473eddf1a809a363e2d017de16744cd
Sébastien Voide <sebastien.voide@eduvaud.ch>
2025-02-05 11:08:42 +0100

    Add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-const app = createApp(App)
+import App from './App.vue';
+import router from './router';
 
-app.use(router)
+const pinia = createPinia();
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+app.use(pinia);
+app.mount('#app');
