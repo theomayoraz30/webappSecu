7cc12b3571d874a2f0d6524549d1e531f70b21c7
Sébastien Voide <sebastienvoide876@gmail.com>
2025-02-12 11:59:40 +0100

    Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,19 +1,114 @@
 <script lang="ts" setup>
+import router from '@/router';
+import { API_REQUEST, API_ROUTES } from '@/scripts/utils/api'
+import { setToken } from '@/scripts/utils/auth'
+import { MESSAGES_ERROR, MESSAGES_TEXT } from '@/scripts/utils/messages';
+import { WEB_ROUTES } from '@/scripts/utils/routes';
+import { ref } from 'vue'
+
+/**
+ * Status du résultat de la requête
+ */
+enum RequestStatus {
+    DEFAULT = 0,
+    SUCCESS = 1,
+    ERROR = 2,
+};
+
+const form = ref({
+    username: '',
+    password: '',
+});
+
+const alert = ref({
+    status: RequestStatus.DEFAULT,
+    message: '',
+});
+
+let waiting = false;
+
+/**
+ * Affiche défaut
+ */
+function showDefaultAlert() {
+    alert.value = {
+        status: RequestStatus.DEFAULT,
+        message: '',
+    };
+};
+
+/**
+ * Affiche succès
+ */
+function showSuccessAlert(message: string) {
+    alert.value = {
+        status: RequestStatus.SUCCESS,
+        message: message,
+    };
+};
+
+/**
+ * Affiche erreur
+ */
+function showErrorAlert(message: string) {
+    alert.value = {
+        status: RequestStatus.ERROR,
+        message: message,
+    };
+};
+
+function login() {
+    if (waiting) return;
+    waiting = true;
+
+    showDefaultAlert();
+
+    const params = {
+        username: form.value.username,
+        password: form.value.password,
+    };
+
+    API_REQUEST.post(API_ROUTES.LOGIN.route, params)
+        .then((response) => {
+            // Check si il manque des données
+            const token = response.data?.data?.token ?? null
+            if (token == null) return showErrorAlert(MESSAGES_ERROR.API_DEFAULT)
+
+            // Enregistre le token dans les cookies
+            setToken(token);
+
+            // Affichage le succès
+            showSuccessAlert(MESSAGES_TEXT.SUCCESS_CONNECTED);
+
+            // Redirection vers la page 'home'
+            router.push({ path: WEB_ROUTES.HOME.path });
+        })
+        .catch((error) => {
+            // Récupére le message d'erreur sinon on prends celui par défaut
+            const message = error.response?.data?.message || MESSAGES_ERROR.API_DEFAULT
+
+            // Affichage de l'erreur
+            showErrorAlert(message)
+        })
+        .finally(() => {
+            waiting = false;
+        });
+}
 </script>
 
 <template>
   <section class="d-flex min-vh-100 position-relative" style="overflow-x: hidden">
     <!-- Top Right Link -->
     <p class="position-absolute top-0 end-0 mt-3 me-4 text-muted">
-        Don't have an account ?
-        <a href="#" class="text-decoration-none signup-link">Sign up</a>
+      Don't have an account ?
+      <a href="#" class="text-decoration-none signup-link">Sign up</a>
     </p>
 
     <!-- Left Side (Illustration) -->
     <div
       class="col-lg-6 d-none d-md-flex align-items-center justify-content-center col-illustration"
     >
-        <img src="@/assets/img/illustration_login.png" alt="Illustration" class="img-fluid" />
+      <img src="@/assets/img/illustration_login.png" alt="Illustration" class="img-fluid" />
     </div>
 
     <!-- Right Side (Login Form) -->
@@ -21,7 +116,7 @@
       <div class="signin-container">
         <h2 class="text-left mb-4">Sign in</h2>
 
-        <form method="post" id="loginForm">
+        <form>
           <div class="mb-3 input-group">
             <span class="input-group-text"><i class="bx bx-user"></i></span>
             <input
@@ -29,6 +124,7 @@
               name="username"
               class="form-control"
               placeholder="username"
+              v-model="form.username"
               required
             />
           </div>
@@ -39,10 +135,11 @@
               name="password"
               class="form-control"
               placeholder="password"
+              v-model="form.password"
               required
             />
           </div>
-          <button type="submit" class="btn btn-primary w-100 btn-lg connexion-btn">
+          <button @click="login" class="btn btn-primary w-100 btn-lg connexion-btn">
             connexion
           </button>
         </form>
